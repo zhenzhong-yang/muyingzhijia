bb9033501ebe3d374465508db2569cdce17e3220
yangzhenzhong <1564594495@qq.com>
2018-05-02 09:00:22 +0800

    购物车

--- src/xiangmu/project/router/router.js
+++ src/xiangmu/project/router/router.js
@@ -4,10 +4,16 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
 import homeComponent from '../components/home/home.vue'
+/*购物车组件*/
+import carComponent from '../components/car/car.vue'
+/*登录组件*/
+import loginComponent from '../components/login/login.vue'
 
 const router = new VueRouter({
     routes: [
-        {path: '/home', component: homeComponent, name: 'home'}
+        {path: '/home', component: homeComponent, name: 'home'},
+        {path: '/car', component: carComponent, name: 'car'},
+        {path: '/login', component: loginComponent, name: 'login'}
     ]
 })
 
