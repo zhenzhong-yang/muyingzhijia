0c605670af8281e239d2b03edcb32e081ef4c076
zengyifan <1042531417@qq.com>
2018-05-02 11:25:49 +0800

    修改

---
+++ src/xiangmu/project/components/lists/lists.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="list">
+        <div class="head">  
+        </div>
+        <ul class="paixu">
+            <li><span>综合排序</span><i>|</i></li>
+            <li><span>销售</span><i>|</i></li>
+            <li class="li3"><span>价格</span><i>|</i></li>
+            <li><span>筛选</span></li>
+        </ul>
+    </div>
+</template>
+
+<script type="text/javascript">
+    import './lists.css'
+
+    export default {
+        components: {
+
+        }
+    }
+    jQuery(function($){
+        $(".paixu").on("click","li",function(){
+            $(this).css({borderBottom:"2px solid #FF6600"}).siblings("li").css({borderBottom:"none"})
+        });
+        
+    })
+</script>
--- src/xiangmu/project/router/router.js
+++ src/xiangmu/project/router/router.js
@@ -4,10 +4,12 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter);
 
 import homeComponent from '../components/home/home.vue'
+import listsComponent from '../components/lists/lists.vue'
 
 const router = new VueRouter({
     routes: [
-        {path: '/home', component: homeComponent, name: 'home'}
+        {path: '/home', component: homeComponent, name: 'home'},
+        {path: '/lists', component: listsComponent, name: 'lists'}
     ]
 })
 
